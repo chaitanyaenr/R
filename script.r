41966288ddeb88ef439d78e9c44f27a3af02a2b8
NAGA RAVI CHAITANYA ELLURI <nelluri@engineyard.com>
2016-03-08 00:41:17 -0700

    Update script.r

--- script.r
+++ script.r
@@ -24,25 +24,8 @@ interface6 <- c(data[data$if. == "6", ])
 interface7 <- c(data[data$if. == "7", ])
 interface9 <- c(data[data$if. == "9", ])
 interface10 <- c(data[data$if. == "10", ])
-interface11 <- c(data[data$if. == "11", ])temporaryFile <- tempfile()
-download.file("https://www.dropbox.com/s/zjf0lm830wzmkwz/snmp-delta.csv?dl=0",
-                  destfile=temporaryFile, method="curl", extra="-L")
-snmpData = read.csv(temporaryFile)
-interface <- c(snmpData$if.)
-outUCastPkts <- c(snmpData$outUCastPkts)
-inOctets <- c(snmpData$inOctets)
-outOctets <- c(snmpData$outOctets)
-inUcastPkts <- c(snmpData$inUcastPkts)
-outNUCastPkts <- c(snmpData$outNUCastPkts)
-inNUcastPkts <- c(snmpData$inNUcastPkts)
-data <- data.frame(if.=interface,outUCastPkts=outUCastPkts,inOctets=inOctets,outOctets=outOctets,inUcastPkts=inUcastPkts,outNUCastPkts=outNUCastPkts,inNUcastPkts=inNUcastPkts )
-interface1 <- sum(data[data$if. == "2","outUCastPkts" ])
-interface1
-interface2 <- c(data[data$if. == "2", ])
-interface3 <- c(data[data$if. == "3", ])
-interface6 <- c(data[data$if. == "6", ])
-interface7 <- c(data[data$if. == "7", ])
-interface9 <- c(data[data$if. == "9", ])
-interface10 <- c(data[data$if. == "10", ])
 interface11 <- c(data[data$if. == "11", ])
+
+
+
 
